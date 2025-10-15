f1b8519691e331f2971c009abb38e1a662fb52f8
Jesther Costinar <jesther.jc15@gmail.com>
2025-10-15 22:47:16 +0800

    first commit

---
+++ src/main/java/com/costinarj/bankingapplicationsystem/BankingApplicationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.costinarj.bankingapplicationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingApplicationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingApplicationSystemApplication.class, args);
+    }
+
+}
